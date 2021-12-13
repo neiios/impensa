bf702af79c63a968830be1e900795f844275949d
Igor <68248740+richard96292@users.noreply.github.com>
2021-12-13 19:42:31 +0200

    Fixed data types for expenses table

--- database.sql
+++ database.sql
@@ -17,10 +17,10 @@ CREATE TABLE users(
 
 CREATE TABLE expenses(
   expense_id UUID DEFAULT uuid_generate_v4(),
-  expense_amount MONEY NOT NULL,
+  expense_amount NUMERIC NOT NULL,
   expense_description VARCHAR(255) NOT NULL,
   expense_category VARCHAR(255) NOT NULL,
-  expense_date date NOT NULL,
+  expense_date timestamptz NOT NULL DEFAULT now(),
   user_id UUID,
   PRIMARY KEY (expense_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)  
@@ -38,6 +38,6 @@ CREATE TABLE expenses(
 
 INSERT INTO users (user_name, user_email, user_password) VALUES ('barbum', 'richard@impensa.studio', 'password');
 
-INSERT INTO expenses (expense_amount, expense_description, expense_category, expense_date, user_id) VALUES (15,'here is a short description', 'category for the expense', CURRENT_DATE, '765c1420-161d-47aa-8c44-5cf855e8e169');
+INSERT INTO expenses (expense_amount, expense_description, expense_category, user_id) VALUES ('11.65','here is a short description', 'category for the expense', '765c1420-161d-47aa-8c44-5cf855e8e169');
 
 -- INSERT INTO categories (category_description, user_id) VALUES ('Netflix', '8566a88a-a2dc-4955-9717-bc3a6e993569');
