9030ca3482b57a082d24a44b76c9c70177e4daff
Nitesh Sharma <81791047+niteshprogrammer@users.noreply.github.com>
2021-12-19 22:57:45 +0530

    Added React Fragment

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -1,11 +1,12 @@
+import { Fragment } from 'react';
 import Welcome from "./Welcome/index.jsx";
 import Navigation from "./Navigation/index.jsx";
 const Header = () => {
   return (
-    <>
+    <Fragment>
       <Navigation />
       <Welcome />
-    </>
+    </Fragment>
   );
 };
 
