c8bd08017db87075de76af0c2fcceeee895efcfa
Igor <68248740+richard96292@users.noreply.github.com>
2022-01-13 15:11:09 +0200

    Added dev script and fix a small typo

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -24,7 +24,7 @@ router.post("/register", validInfo, async (req, res) => {
     ]);
 
     if (user.rows.length > 0) {
-      return res.status(401).json("User already exist!");
+      return res.status(401).json("User already exists!");
     }
 
     const salt = await bcrypt.genSalt(10);
