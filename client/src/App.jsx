9d85496adfa00d8b1a985577bde8a0bd27386844
tmneth <80415416+tmneth@users.noreply.github.com>
2022-01-13 13:53:35 +0200

    Added styles to the toast component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -83,7 +83,17 @@ const App = () => {
           <Route component={PageNotFound}></Route>
         </Switch>
       </BrowserRouter>
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
