0de073b1ce105c13da1117aabf735016cdd4e11e
Igor <68248740+richard96292@users.noreply.github.com>
2022-01-14 13:36:58 +0200

    Fixed immediate redirect to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const checkAuthenticated = async () => {
@@ -27,6 +28,7 @@ const App = () => {
 
       // if user's token is valid sets isAuthenticated to true
       parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      setIsLoading(false);
     } catch (err) {
       console.error(err.message);
     }
@@ -45,44 +47,46 @@ const App = () => {
     <>
       <GlobalStyle />
       <ModalRoot />
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/signin"
-            render={(props) =>
-              !isAuthenticated ? (
-                <SignIn {...props} setAuth={setAuth} />
-              ) : (
-                <Redirect to="/dashboard/overview" />
-              )
-            }
-          />
-          <Route
-            exact
-            path="/signup"
-            render={(props) =>
-              !isAuthenticated ? (
-                <SignUp {...props} setAuth={setAuth} />
-              ) : (
-                <Redirect to="/dashboard/overview" />
-              )
-            }
-          />
-          <Route
-            path="/dashboard"
-            render={(props) =>
-              isAuthenticated ? (
-                <Dashboard {...props} setAuth={setAuth} />
-              ) : (
-                <Redirect to="/signin" />
-              )
-            }
-          />
-          <Route exact path="/" render={() => <Homepage />} />
-          <Route component={PageNotFound}></Route>
-        </Switch>
-      </BrowserRouter>
+      {isLoading ? null : (
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/signin"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <SignIn {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard/overview" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/signup"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <SignUp {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard/overview" />
+                )
+              }
+            />
+            <Route
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/signin" />
+                )
+              }
+            />
+            <Route exact path="/" render={() => <Homepage />} />
+            <Route component={PageNotFound}></Route>
+          </Switch>
+        </BrowserRouter>
+      )}
       <ToastContainer
         position="bottom-right"
         autoClose={1500}
